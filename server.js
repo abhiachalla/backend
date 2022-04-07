84379dfdc9ad31e36d50d6dae89c743c91349270
abhiachalla <70327235+abhiachalla@users.noreply.github.com>
2022-04-06 23:43:33 -0400

    commit

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/', (req, res) => {
+    res.send('Please append /api/tuits to this link to see the tuits!');
+});
  
 require('./services/tuits-service')(app);
 
