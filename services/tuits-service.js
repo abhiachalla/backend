79f6f2f762dca6404573aef275648b48df3da26b
abhiachalla <70327235+abhiachalla@users.noreply.github.com>
2022-04-07 02:42:58 -0400

    commit

--- services/tuits-service.js
+++ services/tuits-service.js
@@ -22,7 +22,8 @@ module.exports = (app) => {
             "stats": {
                 "comments": 123,
                 "retweets": 234,
-                "likes": 345
+                "likes": 345,
+                "dislikes":99
             },
             ...req.body,
         }
@@ -61,6 +62,26 @@ module.exports = (app) => {
         res.sendStatus(200);
     }
     app.put('/api/tuits/:id/like', likeTuit);
+
+    const dislikeTuit = (req, res) => {
+        const id = req.params['id'];
+        tuits = tuits.map(tuit => {
+            if (tuit._id === id) {
+                if (tuit.disliked === true) {
+                    tuit.disliked = false;
+                    tuit.stats.dislikes--;
+                } else {
+                    tuit.disliked = true;
+                    tuit.stats.dislikes++;
+                }
+                return tuit;
+            } else {
+                return tuit;
+            }
+        });
+        res.sendStatus(200);
+    }
+    app.put('/api/tuits/:id/dislike', dislikeTuit);
 
 };
 
