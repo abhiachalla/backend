b7b469c79be243f45547f148e398fba9ea6f659f
abhiachalla <70327235+abhiachalla@users.noreply.github.com>
2022-04-09 17:44:24 -0400

    commit

--- services/users-service.js
+++ services/users-service.js
@@ -1,12 +1,23 @@
 let people = require('../data/users');
 let users = people;
 
-const userController = (app) => {
- app.get('/api/users', findAllUsers);
-}
+module.exports = (app) => {
+    const findAllUsers = (req, res) => {
+        res.json(users);
+       }
+    const userController = (app) => {
+        app.get('/api/users', findAllUsers);
+       }
+       
 
-const findAllUsers = (req, res) => {
- res.json(users);
-}
+};
 
-export default userController;
+
+
+
+ 
+
+
+
+
+
